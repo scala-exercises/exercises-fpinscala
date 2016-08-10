2c8913e6128c0f37ca08731053d0a51e394e31b3
Javier de Silóniz Sandino <javier.s.s@47deg.com>
2016-08-10 12:50:26 +0200

    Added new section to library

---
+++ src/main/scala/fpinscalalib/FunctionalStateSection.scala
@@ -0,0 +1,9 @@
+package fpinscalalib
+
+import org.scalatest.{FlatSpec, Matchers}
+
+/** @param name pure_functional_state
+  */
+object FunctionalStateSection extends FlatSpec with Matchers with org.scalaexercises.definitions.Section {
+
+}
