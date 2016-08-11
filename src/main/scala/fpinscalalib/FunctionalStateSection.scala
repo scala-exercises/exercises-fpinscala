4a7cec9fdde658f850bf9e258414a4e79cec9e84
Javier de Silóniz Sandino <javier.s.s@47deg.com>
2016-08-11 12:44:54 +0200

    Added exercises regarding functional random generators

--- src/main/scala/fpinscalalib/FunctionalStateSection.scala
+++ src/main/scala/fpinscalalib/FunctionalStateSection.scala
@@ -1,9 +1,352 @@
 package fpinscalalib
 
+import fpinscalalib.customlib.state.RNG.Simple
+import fpinscalalib.customlib.state.RNG._
 import org.scalatest.{FlatSpec, Matchers}
+import org.scalacheck.Shapeless._
+import fpinscalalib.customlib.state._
 
 /** @param name pure_functional_state
   */
 object FunctionalStateSection extends FlatSpec with Matchers with org.scalaexercises.definitions.Section {
 
+  /**
+    * == Purely functional random number generation =
+    *
+    * If you need to generate random numbers in Scala, there’s a class in the standard library, `scala.util.Random`,
+    * with a pretty typical imperative API that relies on side effects. Here’s an example of its use:
+    *
+    * {{{
+    *   val rng = new scala.util.Random
+    *
+    *   scala> rng.nextDouble
+    *   res1: Double = 0.9867076608154569
+    *   scala> rng.nextDouble
+    *   res2: Double = 0.8455696498024141
+    * }}}
+    *
+    * As this API doesn't guarantee referential transparency, we need to make the state updates `explicit`. Here's one
+    * possible interface to a random number generator:
+    *
+    * {{{
+    *   trait RNG {
+    *     def nextInt: (Int, RNG)
+    *   }
+    * }}}
+    *
+    * The following is a simple random number generator that uses the same algorithm as `scala.util.Random`. The details
+    * of this implementation aren’t really important, but notice that `nextInt` returns both the generated value and a
+    * new `RNG` to use for generating the next value.
+    *
+    * {{{
+    *   case class Simple(seed: Long) extends RNG {
+    *     def nextInt: (Int, RNG) = {
+    *       // `&` is bitwise AND. We use the current seed to generate a new seed.
+    *       val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+    *       // The next state, which is an `RNG` instance created from the new seed.
+    *       val nextRNG = Simple(newSeed)
+    *       // `>>>` is right binary shift with zero fill. The value `n` is our new pseudo-random integer.
+    *       val n = (newSeed >>> 16).toInt
+    *       // The return value is a tuple containing both a pseudo-random integer and the next `RNG` state.
+    *       (n, nextRNG)
+    *       }
+    *    }
+    * }}}
+    *
+    * Here's an example of using this API from the interpreter:
+    *
+    * {{{
+    *   scala> val rng = SimpleRNG(42)
+    *   rng: SimpleRNG = SimpleRNG(42)
+    *
+    *   scala> val (n1, rng2) = rng.nextInt
+    *   n1: Int = 16159453
+    *   rng2: RNG = SimpleRNG(1059025964525)
+    *
+    *   scala> val (n2, rng3) = rng2.nextInt
+    *   n2: Int = -1281479697
+    *   rng3: RNG = SimpleRNG(197491923327988)
+    * }}}
+    *
+    * We can run this sequence of statements as many times as we want and we’ll always get the same values. When
+    * we call `rng.nextInt`, it will always return `16159453` and a new `RNG`, whose `nextInt` will always return
+    * `-1281479697`. In other words, this API is pure.
+    *
+    * = Making stateful APIs pure =
+    *
+    * This problem of making seemingly stateful APIs pure comes up frequently, and we can always deal with it in this
+    * same way. For instance:
+    *
+    * {{{
+    *   class Foo {
+    *     private var s: FooState = ...
+    *     def bar: Bar
+    *     def baz: Int
+    *   }
+    * }}}
+    *
+    * Supposing `bar` and `baz` each mutate `s` in some way, we can translate this to the following alternative `trait`,
+    * making the caller responsible for passing the computed next state through the rest of the program:
+    *
+    * {{{
+    *   trait Foo {
+    *     def bar: (Bar, Foo)
+    *     def baz: (Int, Foo)
+    *   }
+    * }}}
+    *
+    * Let's write a function that uses `RNG.nextInt` to generate a random integer between `0` and `Int.maxValue`, making
+    * sure to handle the corner case when `nextInt` returns `Int.MinValue`, which doesn't have a non-negative
+    * counterpart:
+    */
+
+  def randomNonNegativeIntAssert(res0: Int, res1: Int): Unit = {
+    def nonNegativeInt(rng: RNG): (Int, RNG) = {
+      val (i, r) = rng.nextInt
+      (if (i < res0) -(i + res1) else i, r)
+    }
+
+    val rng = Simple(47)
+    val (r1, rng1) = nonNegativeInt(rng)
+    val r2 = nonNegativeInt(rng1)._1
+
+    r1 should be >= 0
+    r2 should be >= 0
+    r1 should not be r2
+  }
+
+  /**
+    * Now let's write a function to generate a `Double` between `0` and `1`, not including `1`. Note the use of
+    * `Int.MaxValue` to divide a random positive integer:
+    */
+
+  def randomDoubleAssert(res0: Int): Unit = {
+    def double(rng: RNG): (Double, RNG) = {
+      val (i, r) = nonNegativeInt(rng)
+      (i / (Int.MaxValue.toDouble + res0), r)
+    }
+
+    val rng = Simple(47)
+    val (d1, rng1) = double(rng)
+    val d2 = double(rng1)._1
+
+    d1.toInt should be >= 0
+    d2.toInt should be >= 0
+    d1 should not be d2
+  }
+
+  /**
+    * Following the same principle, we're going to write functions that generate tuples of random values, i.e.: an
+    * `(Int, Double)` pair, a `(Double, Int)` pair, and a `(Double, Double, Double)` 3-tuple, by reusing the functions
+    * we've already written:
+    *
+    * {{{
+    *
+    *   def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    *     val (i, r1) = rng.nextInt
+    *     val (d, r2) = double(r1)
+    *     ((i, d), r2)
+    *   }
+    *
+    *   def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    *     val ((i, d), r) = intDouble(rng)
+    *     ((d, i), r)
+    *   }
+    *
+    *   def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    *     val (d1, r1) = double(rng)
+    *     val (d2, r2) = double(r1)
+    *     val (d3, r3) = double(r2)
+    *     ((d1, d2, d3), r3)
+    *   }
+    * }}}
+    *
+    * We can even go a bit beyond and write a function to generate a list of random integers:
+    */
+
+  def randomIntListAssert(res0: Int, res1: Int): Unit = {
+    def ints(count: Int)(rng: RNG): (List[Int], RNG) =
+      if (count == res0)
+        (List(), rng)
+      else {
+        val (x, r1)  = rng.nextInt
+        val (xs, r2) = ints(count - res1)(r1)
+        (x :: xs, r2)
+      }
+
+    val (l1, r1) = ints(5)(Simple(47))
+    val l2 = ints(5)(r1)._1
+    l1.size shouldBe 5
+    l1.headOption should not be l2
+  }
+
+  /**
+    * To simplify the pattern we've been seeing so far, and to avoid passing the state along each time we execute these
+    * state actions, let's make a type alias for the `RNG` state action data type:
+    *
+    * {{{
+    *   type Rand[+A] = RNG => (A, RNG)
+    * }}}
+    *
+    * We can now turn methods such as `RNG`'s `nextInt` into values of this new type:
+    *
+    * {{{
+    *   val int: Rand[Int] = _.nextInt
+    * }}}
+    *
+    * We want to write combinators that let us combine `Rand` actions while avoiding explicitly passing along the `RNG`
+    * state. We’ll end up with a kind of domain-specific language that does all of the passing for us. For example, a
+    * simple `RNG` state transition is the `unit` action, which passes the `RNG` state through without using it, always
+    * returning a constant value rather than a random value:
+    *
+    * {{{
+    *   def unit[A](a: A): Rand[A] = rng => (a, rng)
+    * }}}
+    *
+    * There’s also `map` for transforming the output of a state action without modifying the state itself. Remember,
+    * `Rand[A]` is just a type alias for a function type `RNG => (A, RNG)`, so this is just a kind of function
+    * composition:
+    *
+    * {{{
+    *   def map[A,B](s: Rand[A])(f: A => B): Rand[B] = rng => {
+    *     val (a, rng2) = s(rng)
+    *     (f(a), rng2)
+    *   }
+    * }}}
+    *
+    * As an example of how `map` is used, let’s write `nonNegativeEven`, which reuses `nonNegative Int` to generate an
+    * `Int` that’s greater than or equal to zero and divisible by two:
+    */
+
+  def randomNonNegativeEvenAssert(res0: Int): Unit = {
+    def nonNegativeEven: Rand[Int] = map(nonNegativeInt)(i => i - i % res0)
+
+    val (r1, rng1) = nonNegativeEven(Simple(47))
+    val r2 = nonNegativeEven(rng1)._1
+
+    r1 % 2 shouldBe 0
+    r2 % 2 shouldBe 0
+    r1 should not be r2
+  }
+
+  /**
+    * We can also use `map` to reimplement `double` in a more elegant way:
+    */
+
+  def randomDoubleViaMap(res0: Int): Unit = {
+    val double: Rand[Double] =
+      map(nonNegativeInt)(_ / (Int.MaxValue.toDouble + res0))
+
+    val rng = Simple(47)
+    val (d1, rng1) = double(rng)
+    val d2 = double(rng1)._1
+
+    d1.toInt should be >= 0
+    d2.toInt should be >= 0
+    d1 should not be d2
+  }
+
+  /**
+    * Now we're going to implement `map2`, that will allow us to implement functions like `intDouble` and `doubleInt`.
+    * This function takes two actions, `ra` and `rb`, and a binary function `f` for combining their results, and returns
+    * a new action that combines them:
+    *
+    * {{{
+    *   def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    *     rng => {
+    *       val (a, r1) = ra(rng)
+    *       val (b, r2) = rb(r1)
+    *       (f(a, b), r2)
+    *     }
+    * }}}
+    *
+    * If we have an action that generates values of type `A` and an action to generate values of type `B`, then we can
+    * combine them into one action that generates pairs of both `A` and `B`:
+    *
+    * {{{
+    *   def both[A,B](ra: Rand[A], rb: Rand[B]): Rand[(A,B)] = map2(ra, rb)((_, _))
+    * }}}
+    *
+    * We can use this to reimplement `intDouble` and `doubleInt` from exercise 6.3 more succinctly:
+    *
+    * {{{
+    *   val randIntDouble: Rand[(Int, Double)] = both(int, double)
+    *
+    *   val randDoubleInt: Rand[(Double, Int)] = both(double, int)
+    * }}}
+    *
+    * If we can combine two `RNG` transitions, we should be able to combine a whole list of them. Let's implement
+    * `sequence` for combining a `List` of transitions into a single transition:
+    *
+    * {{{
+    *   def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
+    *     fs.foldRight(unit(List[A]()))((f, acc) => map2(f, acc)(_ :: _))
+    * }}}
+    *
+    * We can use `sequence` to reimplement the `ints` function we wrote before. To do so, let's use the standard library
+    * function `List.fill(n)(x)` to make a list with `x` repeated `n` times:
+    *
+    * {{{
+    *   def _ints(count: Int): Rand[List[Int]] =
+    *     sequence(List.fill(count)(int))
+    * }}}
+    *
+    * Our next step is to implement `flatMap`, as it will allow us to change state operations:
+    *
+    * {{{
+    *   def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] =
+    *     rng => {
+    *       val (a, r1) = f(rng)
+    *       g(a)(r1)
+    *     }
+    * }}}
+    *
+    * Let's use it to write `nonNegativeLessThan`, which generates an integer between `0` (inclusive) and `n`
+    * (exclusive):
+    */
+
+  def randomNonNegativeLessThan(res0: Int, res1: Int): Unit = {
+    def nonNegativeLessThan(n: Int): Rand[Int] = {
+      flatMap(nonNegativeInt) { i =>
+        val mod = i % n
+        if (i + (n - res0) - mod >= res1) unit(mod) else nonNegativeLessThan(n)
+      }
+    }
+
+    val (r1, rng1) = nonNegativeLessThan(10)(Simple(47))
+    val r2 = nonNegativeLessThan(10)(rng1)._1
+
+    r1 should be >= 0
+    r1 should be < 10
+    r2 should be >= 0
+    r2 should be < 10
+    r1 should not be r2
+  }
+
+  /**
+    * We can also rewrite `map` and `map2` in terms of `flatMap`. The fact that this is possible is what we’re referring
+    * to when we say that `flatMap` is more powerful than `map` and `map2`.
+    *
+    * {{{
+    *   def _map[A,B](s: Rand[A])(f: A => B): Rand[B] =
+    *     flatMap(s)(a => unit(f(a)))
+    *    def _map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    *     flatMap(ra)(a => map(rb)(b => f(a, b)))
+    * }}}
+    *
+    * As a final example, let's revisit our previously written functions to implement a function that roll a six-sided
+    * die:
+    */
+
+  def randomRollDie(res0: Int): Unit = {
+    def rollDie: Rand[Int] = map(nonNegativeLessThan(6))(_ + res0)
+
+    val (d1, r1) = rollDie(Simple(47))
+    val d2 = rollDie(r1)._1
+    d1 should be > 0
+    d1 should be < 6
+    d2 should be > 0
+    d2 should be < 6
+    d1 should not be d2
+  }
 }
