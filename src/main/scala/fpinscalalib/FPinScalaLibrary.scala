328a0a6ef4a2c75dacbf864fa329984ded09220f
Javier de Silóniz Sandino <javier.s.s@47deg.com>
2016-09-16 06:09:16 +0200

    Fix library short title

--- src/main/scala/fpinscalalib/FPinScalaLibrary.scala
+++ src/main/scala/fpinscalalib/FPinScalaLibrary.scala
@@ -2,9 +2,7 @@ package fpinscalalib
 
 import org.scalaexercises.definitions._
 
-/** Exercises based on Manning's "Functional Programming in Scala" by by Paul Chiusano and Rúnar Bjarnason.
-  * For more information about the book please visit its
-  * <a href="https://www.manning.com/books/functional-programming-in-scala">official website</a>.
+/** Exercises based on Manning's "Functional Programming in Scala" book by Paul Chiusano and Rúnar Bjarnason.
   *
   * @param name fp_in_scala
   */
