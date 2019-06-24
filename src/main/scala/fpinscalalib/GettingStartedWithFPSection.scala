537513f7a0c5467aaebfb81f7f8df5d440b48cc3
Javier de Siloniz Sandino <jsandino@starbuck.com>
2019-06-24 06:02:18 -0700

    fix

--- src/main/scala/fpinscalalib/GettingStartedWithFPSection.scala
+++ src/main/scala/fpinscalalib/GettingStartedWithFPSection.scala
@@ -56,14 +56,14 @@ object GettingStartedWithFPSection
       @annotation.tailrec
       def go(n: Int): Boolean =
         if (n >= as.length - 1) true
-        else if (ordering(as(n), as(n + 1))) false
+        else if (!ordering(as(n), as(n + 1))) false
         else go(n + 1)
 
       go(0)
     }
 
-    isSorted(Array(1, 3, 5, 7), (x: Int, y: Int) => x > y) shouldBe res0
-    isSorted(Array(7, 5, 1, 3), (x: Int, y: Int) => x < y) shouldBe res1
+    isSorted(Array(1, 3, 5, 7), (x: Int, y: Int) => x < y) shouldBe res0
+    isSorted(Array(7, 5, 1, 3), (x: Int, y: Int) => x > y) shouldBe res1
     isSorted(Array("Scala", "Exercises"), (x: String, y: String) => x.length > y.length) shouldBe res2
   }
 
