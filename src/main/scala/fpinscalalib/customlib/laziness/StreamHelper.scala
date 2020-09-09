6f238650493b0450625b686fef38242559d36cd5
47erbot <developer@47deg.com>
2020-09-09 07:12:56 +0200

    Update scalafmt-core to 2.7.0

--- src/main/scala/fpinscalalib/customlib/laziness/StreamHelper.scala
+++ src/main/scala/fpinscalalib/customlib/laziness/StreamHelper.scala
@@ -195,8 +195,8 @@ trait Stream[+A] {
   `s startsWith s2` when corresponding elements of `s` and `s2` are all equal, until the point that `s2` is exhausted. If `s` is exhausted first, or we find an element that doesn't match, we terminate early. Using non-strictness, we can compose these three separate logical steps--the zipping, the termination when the second stream is exhausted, and the termination if a nonmatching element is found or the first stream is exhausted.
    */
   def startsWith[T](s: Stream[T]): Boolean =
-    zipAll(s).takeWhile(_._2.isDefined) forAll {
-      case (h, h2) => h == h2
+    zipAll(s).takeWhile(_._2.isDefined) forAll { case (h, h2) =>
+      h == h2
     }
 
   /*
